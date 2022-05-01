00ed7e5bfb212c2503a5b3af8ecf96943b4644ad
Unreasonable motto <90086313+Athul1223@users.noreply.github.com>
2022-05-01 13:08:42 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,38 +1,33 @@
-#(©)Codexbotz
+#(©)CodeXBotz
 import os
 import asyncio
 from pyrogram import Client, filters, __version__
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 
-from bot import Alina
-from config import ADMINS, FORCE_MSG, START_MSG, OWNER_ID, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, DB_URL, SESSION
+from bot import Bot
+from config import ADMINS, FORCE_MSG, START_MSG, OWNER_ID, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT
 from helper_func import subscribed, encode, decode, get_messages
-from Database.Database import db
-from plugins.Broadcast import broadcast
-
+from database.sql import add_user, query_msg, full_userbase
 
 
 #=====================================================================================##
-
-
 
 WAIT_MSG = """"<b>Processing ...</b>"""
 
 REPLY_ERROR = """<code>Use this command as a replay to any telegram message with out any spaces.</code>"""
 
-
 #=====================================================================================##
 
 
-@Alina.on_message(filters.command('start') & filters.private & subscribed)
+@Bot.on_message(filters.command('start') & filters.private & subscribed)
 async def start_command(client: Client, message: Message):
-    chat_id = message.from_user.id
-    if not await db.is_user_exist(chat_id):
-        data = await client.get_me()
-        BOT_USERNAME = data.username
-        await db.add_user(chat_id)
-    
+    id = message.from_user.id
+    user_name = '@' + message.from_user.username if message.from_user.username else None
+    try:
+        await add_user(id, user_name)
+    except:
+        pass
     text = message.text
     if len(text)>7:
         try:
@@ -73,9 +68,7 @@ async def start_command(client: Client, message: Message):
         for msg in messages:
 
             if bool(CUSTOM_CAPTION) & bool(msg.document):
-                caption = CUSTOM_CAPTION.format(previouscaption = '' if not msg.caption else msg.caption.html, filename = msg.document.file_name.replace("_", "."))
-            elif bool(CUSTOM_CAPTION) & bool(msg.video):
-                caption = CUSTOM_CAPTION.format(previouscaption = '' if not msg.caption else msg.caption.html, filename = msg.video.file_name.replace("_", "."))
+                caption = CUSTOM_CAPTION.format(previouscaption = "" if not msg.caption else msg.caption.html, filename = msg.document.file_name)
             else:
                 caption = "" if not msg.caption else msg.caption.html
 
@@ -85,11 +78,11 @@ async def start_command(client: Client, message: Message):
                 reply_markup = None
 
             try:
-                await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = 'html', reply_markup = reply_markup)
-                await asyncio.sleep(0.3)
+                await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = 'html', reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
+                await asyncio.sleep(0.5)
             except FloodWait as e:
                 await asyncio.sleep(e.x)
-                await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = 'html', reply_markup = reply_markup)
+                await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = 'html', reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
             except:
                 pass
         return
@@ -97,7 +90,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("✨ INFAME SERIES ✨", url="https://t.me/InfameSeries"),
+                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
+                    InlineKeyboardButton("🔒 Close", callback_data = "close")
                 ]
             ]
         )
@@ -115,7 +109,7 @@ async def start_command(client: Client, message: Message):
         )
         return
 
-@Alina.on_message(filters.command('start') & filters.private)
+@Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
     buttons = [
         [
@@ -147,25 +141,4 @@ async def not_joined(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(buttons),
         quote = True,
         disable_web_page_preview = True
-    )
-
-@Alina.on_message(filters.private & filters.command("broadcast"))
-async def broadcast_handler_open(_, m):
-    if m.from_user.id not in ADMINS:
-        await m.delete()
-        return
-    if m.reply_to_message is None:
-        await m.delete()
-    else:
-        await broadcast(m, db)
-
-@Alina.on_message(filters.private & filters.command("users"))
-async def sts(c, m):
-    if m.from_user.id not in ADMINS:
-        await m.delete()
-        return
-    await m.reply_text(
-        text=f"**Total subscribers:** `{await db.total_users_count()}`\n\n**Users:** `{await db.total_notif_users_count()}` \n **Blocked/deleted :** `{await db.total_users_count() - await db.total_notif_users_count()}`",
-        parse_mode="Markdown",
-        quote=True
     )
